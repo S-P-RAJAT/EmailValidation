36949b4d75f6185754a326cd7376ed5ecd89c587
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-07 20:42:03 +0530

    MODIFIED: Check for TLD for being at least two letters

--- src/com/bridgelabz/regex/EmailValidator.java
+++ src/com/bridgelabz/regex/EmailValidator.java
@@ -2,6 +2,6 @@ package com.bridgelabz.regex;
 
 public class EmailValidator {
 	public boolean validate(String email) {
-		return email.matches("^abc?(?(.)[a-z0-9_+-]+)@bridgelabz.co?(.)[a-z]*$");
+		return email.matches("^abc?(?(.)[a-z0-9_+-]+)@bridgelabz.co?(.)[a-z]{2,}$");
 	}
 }
